d6ff316670ecc0f821401f3e30340a3551b0e09e
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-08-10 19:37:19 +0900

    .
    .

--- js/cWriteSch.js
+++ js/cWriteSch.js
@@ -4,6 +4,14 @@ $(function() {
         showMonthAfterYear:true,
         changeYear: true, //콤보박스에서 년 선택 가능
         changeMonth: true,
-        
+        prevText: '이전 달',
+        nextText: '다음 달',
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+        showMonthAfterYear: true,
+        yearSuffix: '년',
     });
 });
