83519442b0460d8505ccb39efc5194aeeb673940
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-08-10 19:24:40 +0900

    .
    .

--- js/cWriteSch.js
+++ js/cWriteSch.js
@@ -1,3 +1,9 @@
 $(function() {
-    $(".datePick").datepicker();
+    $(".datePick").datepicker({
+        dateFormat: 'yy-mm-dd',
+        showMonthAfterYear:true,
+        changeYear: true, //콤보박스에서 년 선택 가능
+        changeMonth: true,
+        
+    });
 });
