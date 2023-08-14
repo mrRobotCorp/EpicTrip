89c7b318437bd536e6e327c8c9570d00e5975f69
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-08-14 13:37:14 +0900

    .
    .

--- js/cWriteSch.js
+++ js/cWriteSch.js
@@ -2,18 +2,13 @@
 $(function () {
     $(".datePick").datepicker({
         dateFormat: 'yy-mm-dd',
-        showMonthAfterYear: true,
-        changeYear: true, //콤보박스에서 년 선택 가능
-        changeMonth: true,
-        prevText: '이전 달',
-        nextText: '다음 달',
         monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
         monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
         dayNames: ['일', '월', '화', '수', '목', '금', '토'],
         dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
         dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
         showMonthAfterYear: true,
-        yearSuffix: '년',
+        yearSuffix: '년'
     });
 });
 
@@ -49,4 +44,27 @@ function initMap() {
 
 window.initMap = initMap;
 
+
+// ------------------ modal ---------------------
+const modal = document.querySelector(".modalBg");
+const clsBtn = document.querySelector(".cls");
+const locAdd = document.querySelectorAll(".locAdd");
+
+function showModal() {
+    modal.classList.add("active")
+}
+
+function clsModal() {
+    modal.classList.remove("active");
+}
+
+locAdd.forEach((e) => {
+    e.onclick = function() {
+        showModal();
+    };
+});
+
+clsBtn.onclick = function() {
+    clsModal();
+};
 
